dc3cedee71ad25a1c62a852780fba3c974ebf5b7
keyready <vip.korchak@list.ru>
2024-12-14 12:34:57 +0300

    убрал global.d.ts

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-    readonly DEV: boolean;
-    readonly VITE_PASSWORD_PUBKEY: string;
-    readonly VITE_PASSWORD_PRIVKEY: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
